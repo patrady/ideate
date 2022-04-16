7698d5f57a42bbb9d3423a1992f0bc6fd5bdf398
Patrick Brady <patrick.brady@daveramsey.com>
2022-04-16 11:00:20 -0500

    Add error state to input

--- components/rds/input.tsx
+++ components/rds/input.tsx
@@ -4,7 +4,7 @@ import { useField } from "formik";
 import styles from "./rds.module.scss";
 
 type InputProps = {
-    className?: string;
+  className?: string;
   name: string;
   label: string;
   help?: string;
@@ -13,20 +13,40 @@ type InputProps = {
 export default function Input(props: InputProps) {
   const { className, name, label, help } = props;
   const [{ value, onChange, onBlur }, { touched, error }] = useField(name);
+  const hasError = touched && error;
 
   return (
-    <div className={clsx(className, styles["rds-FormField"])}>
-      <Text element="label" className={styles["rds-FormField-label"]} htmlFor={name}>
+    <div className={clsx(styles["rds-FormField"], className)}>
+      <Text
+        element="label"
+        className={styles["rds-FormField-label"]}
+        htmlFor={name}
+      >
         {label}
       </Text>
-      <div className={styles["rds-FormField-control"]}>
+      <div
+        className={clsx(styles["rds-FormField-control"], {
+          [styles["rds-FormField-control--error"]]: hasError,
+        })}
+      >
         <div className={styles["rds-Input"]}>
-          <input name={name} value={value} onChange={onChange} onBlur={onBlur} />
+          <input
+            name={name}
+            value={value}
+            onChange={onChange}
+            onBlur={onBlur}
+          />
         </div>
       </div>
-      {help && <Text className={styles["rds-FormField-help"]} color="subdued">{help}</Text>}
-      {touched && error && (
-        <Text element="span" className={styles["rds-FormField-error"]}>{error}</Text>
+      {hasError && (
+        <Text element="span" className={styles["rds-FormField-errorMessage"]}>
+          {error}
+        </Text>
+      )}
+      {help && (
+        <Text className={styles["rds-FormField-help"]} color="subdued">
+          {help}
+        </Text>
       )}
     </div>
   );

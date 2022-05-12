aa6ea407d58e3bb364a2b041888638aa203a6db6
Patrick Brady <patrick.brady@daveramsey.com>
2022-05-12 17:42:55 -0500

    Add tags to cards

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,10 +1,12 @@
 import { useDrag } from "react-dnd";
 import Text from "@ramsey-design-system/text";
-import { Card as CardModel } from "../../models";
-import styles from "./card.module.scss";
 import Button from "@ramsey-design-system/button";
+import Stack from "@ramsey-design-system/stack";
+import Tag from "@ramsey-design-system/tag";
+import { Card as CardModel } from "../../models";
 import { EditOutlinedIcon } from "..";
 import { TimeOutlined } from "../icons/icons";
+import styles from "./card.module.scss";
 
 type CardProps = {
   card: CardModel;
@@ -35,14 +37,23 @@ export default function Card(props: CardProps) {
           onClick={handleEdit}
         />
       </div>
-      {card.showTime() && (
-        <div className={styles["Card-time"]}>
-          <TimeOutlined className={styles["Card-time-icon"]} />
-          <Text className={styles["Card-time-text"]} size="bodySmall">
-            {`${card.getTestStartDate()} - ${card.getTestEndDate()}`}
-          </Text>
-        </div>
-      )}
+      <div className={styles["Card-footer"]}>
+        {card.showTime() && (
+          <div className={styles["Card-time"]}>
+            <TimeOutlined className={styles["Card-time-icon"]} />
+            <Text className={styles["Card-time-text"]} size="bodySmall">
+              {`${card.getTestStartDate()} - ${card.getTestEndDate()}`}
+            </Text>
+          </div>
+        )}
+        {card.tags.length > 0 && (
+          <Stack spacing="small">
+            {card.tags.map((tag, i) => (
+              <Tag key={i} text={tag} color="primary" subtle />
+            ))}
+          </Stack>
+        )}
+      </div>
     </div>
   );
 }
